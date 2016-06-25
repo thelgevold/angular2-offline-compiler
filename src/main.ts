eec83affb2ea5e18778ffbbab686aa44e94e8708
Torgeir Helgevold <torgeirh@hotmail.com>
2016-06-25 00:09:31 -0400

    remove forms

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import {ComponentResolver, ReflectiveInjector, coreBootstrap} from '@angular/core';
 import {BROWSER_APP_PROVIDERS, browserPlatform} from '@angular/platform-browser';
 
-import {provideForms, disableDeprecatedForms} from '@angular/forms';
-
 //offline compiled entry point
 import {AppComponentNgFactory} from './app/app.component.ngfactory';
 
-const appInjector = ReflectiveInjector.resolveAndCreate([BROWSER_APP_PROVIDERS, provideForms()], browserPlatform().injector);
+const appInjector = ReflectiveInjector.resolveAndCreate([BROWSER_APP_PROVIDERS], browserPlatform().injector);
 coreBootstrap(AppComponentNgFactory, appInjector);
